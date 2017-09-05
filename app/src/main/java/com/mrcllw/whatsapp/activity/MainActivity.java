a1b3ff3333776624de6ae882fe467c215e27e3b8
Marcello <mrcllw@Marcellos-iMac.local>
2017-09-05 11:55:50 -0400

    Tela de login e de Validação

--- app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
@@ -1,10 +1,11 @@
-package com.mrcllw.whatsapp;
+package com.mrcllw.whatsapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mrcllw.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/mrcllw/whatsapp/activity/ValidadorActivity.java
@@ -0,0 +1,15 @@
+package com.mrcllw.whatsapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mrcllw.whatsapp.R;
+
+public class ValidadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validador);
+    }
+}
