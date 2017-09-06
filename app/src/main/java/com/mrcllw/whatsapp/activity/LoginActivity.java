524264405ebb9e6d1be8f9f17a2cbbff3e1fb388
Marcello <mrcllw@Marcellos-iMac.local>
2017-09-06 11:31:21 -0400

    Permissões em tempo de execução

--- app/src/main/java/com/mrcllw/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/mrcllw/whatsapp/activity/LoginActivity.java
@@ -1,5 +1,9 @@
 package com.mrcllw.whatsapp.activity;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
@@ -10,6 +14,7 @@ import android.widget.EditText;
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.mrcllw.whatsapp.R;
+import com.mrcllw.whatsapp.helper.Permissao;
 import com.mrcllw.whatsapp.helper.Preferencias;
 
 import java.util.HashMap;
@@ -20,10 +25,15 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTelefone, editCodEstado, editCodPais, editNome;
     private Button buttonCadastrar;
 
+    private String[] permissoes = new String[]{
+            Manifest.permission.SEND_SMS
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Permissao.validaPermissoes(1, this, permissoes);
 
         editTelefone = (EditText) findViewById(R.id.editTelefone);
         editCodEstado = (EditText) findViewById(R.id.editCodEstado);
@@ -62,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 String mensagem = "WhatsApp código de confirmação: " + token;
                 telefoneSemChar = "8135";
-                enviarSMS("+" + telefoneSemChar, mensagem);
+                boolean enviadoSMS = enviarSMS("+" + telefoneSemChar, mensagem);
 
                 HashMap<String, String> usuarioPreferencias = preferencias.getUsuarioPreferencias();
             }
@@ -73,9 +83,33 @@ public class LoginActivity extends AppCompatActivity {
         try{
             SmsManager smsManager = SmsManager.getDefault();
             smsManager.sendTextMessage(telefone, null, mensagem, null, null);
+            return true;
         } catch (Exception e){
             e.printStackTrace();
+            return false;
         }
-        return true;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int result : grantResults){
+            if(result == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar esse app, é necessario aceitar as permissões.");
+        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
---
+++ app/src/main/java/com/mrcllw/whatsapp/helper/Permissao.java
@@ -0,0 +1,40 @@
+package com.mrcllw.whatsapp.helper;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mrcllw on 06/09/17.
+ */
+
+public class Permissao {
+
+    public static boolean validaPermissoes(int requestCode, Activity activity, String[] permissoes){
+        if(Build.VERSION.SDK_INT >= 23){
+            List<String> listaPermissoes = new ArrayList<String>();
+
+            for(String permissao : permissoes){
+                Boolean validaPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                if(!validaPermissao){
+                    listaPermissoes.add(permissao);
+                }
+            }
+
+            if(listaPermissoes.isEmpty()){
+                return true;
+            }
+
+            String[] novasPermissoes = new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
+        }
+        return true;
+    }
+}
