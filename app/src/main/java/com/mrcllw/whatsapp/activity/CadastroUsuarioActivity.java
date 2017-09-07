3df7e8743d0b478c01a22dd944c9ea5d0a62eec0
Marcello <mrcllw@Marcellos-iMac.local>
2017-09-07 11:15:24 -0400

    Nova tela de login e tela de cadastro de usuário; Cadastro de usuário com FirebaseAuth e cadastro de dados do usuário com FirebaseDatabase

---
+++ app/src/main/java/com/mrcllw/whatsapp/activity/CadastroUsuarioActivity.java
@@ -0,0 +1,66 @@
+package com.mrcllw.whatsapp.activity;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mrcllw.whatsapp.R;
+import com.mrcllw.whatsapp.model.Usuario;
+
+public class CadastroUsuarioActivity extends AppCompatActivity {
+
+    private EditText editCadNome, editCadEmail, editCadSenha;
+    private Button buttonCadastrar;
+
+    private Usuario usuario;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_usuario);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        editCadNome = (EditText) findViewById(R.id.editCadNome);
+        editCadEmail = (EditText) findViewById(R.id.editCadEmail);
+        editCadSenha = (EditText) findViewById(R.id.editCadSenha);
+        buttonCadastrar = (Button) findViewById(R.id.buttonCadastrar);
+
+        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = new Usuario();
+                usuario.setNome(editCadNome.getText().toString());
+                usuario.setEmail(editCadEmail.getText().toString());
+                usuario.setSenha(editCadSenha.getText().toString());
+                cadastrarUsuario();
+            }
+        });
+    }
+
+    private void cadastrarUsuario(){
+        firebaseAuth.createUserWithEmailAndPassword(usuario.getEmail(), usuario.getSenha())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    usuario.setId(task.getResult().getUser().getUid().toString());
+                    usuario.salvarUsuario();
+                    finish();
+                    Toast.makeText(CadastroUsuarioActivity.this, "Cadastrado!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(CadastroUsuarioActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/mrcllw/whatsapp/activity/LoginActivity.java
+++ app/src/main/java/com/mrcllw/whatsapp/activity/LoginActivity.java
@@ -1,123 +1,38 @@
 package com.mrcllw.whatsapp.activity;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.github.rtoshiro.util.format.SimpleMaskFormatter;
-import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 import com.mrcllw.whatsapp.R;
-import com.mrcllw.whatsapp.helper.Permissao;
-import com.mrcllw.whatsapp.helper.Preferencias;
 
-import java.util.HashMap;
-import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editTelefone, editCodEstado, editCodPais, editNome;
-    private Button buttonCadastrar;
-
-    private String[] permissoes = new String[]{
-            Manifest.permission.SEND_SMS
-    };
+    private EditText editEmail, editSenha;
+    private TextView textCadastrar;
+    private Button buttonLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Permissao.validaPermissoes(1, this, permissoes);
 
-        editTelefone = (EditText) findViewById(R.id.editTelefone);
-        editCodEstado = (EditText) findViewById(R.id.editCodEstado);
-        editCodPais = (EditText) findViewById(R.id.editCodPais);
-        editNome = (EditText) findViewById(R.id.editNome);
-        buttonCadastrar = (Button) findViewById(R.id.buttonCadastrar);
+        editEmail = (EditText) findViewById(R.id.editEmail);
+        editSenha = (EditText) findViewById(R.id.editSenha);
+        textCadastrar = (TextView) findViewById(R.id.textCadastrar);
+        buttonLogar = (Button) findViewById(R.id.buttonLogar);
 
-        SimpleMaskFormatter telefoneSMF = new SimpleMaskFormatter("NNNNN-NNNN");
-        SimpleMaskFormatter codEstadoSMF = new SimpleMaskFormatter("NN");
-        SimpleMaskFormatter codPaisSMF = new SimpleMaskFormatter("+NN");
-
-        MaskTextWatcher telefoneMTW = new MaskTextWatcher(editTelefone, telefoneSMF);
-        MaskTextWatcher codEstadoMTW = new MaskTextWatcher(editCodEstado, codEstadoSMF);
-        MaskTextWatcher codPaisMTW = new MaskTextWatcher(editCodPais, codPaisSMF);
-
-        editTelefone.addTextChangedListener(telefoneMTW);
-        editCodEstado.addTextChangedListener(codEstadoMTW);
-        editCodPais.addTextChangedListener(codPaisMTW);
-
-        buttonCadastrar.setOnClickListener(new View.OnClickListener() {
+        textCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nomeUsuario = editNome.getText().toString();
-                String telefoneCompleto = editCodPais.getText().toString() +
-                        editCodEstado.getText().toString() +
-                        editTelefone.getText().toString();
-
-                String telefoneSemChar = telefoneCompleto.replace("+", "").replace("-", "");
-
-                Random random = new Random();
-                int tokenRandom = random.nextInt(8999)+1000;
-                String token = String.valueOf(tokenRandom);
-
-                Preferencias preferencias = new Preferencias(getApplicationContext());
-                preferencias.salvarUsuarioPreferencias(nomeUsuario, telefoneSemChar, token);
-
-                String mensagem = "WhatsApp código de confirmação: " + token;
-                telefoneSemChar = "8135";
-                boolean enviadoSMS = enviarSMS("+" + telefoneSemChar, mensagem);
-
-                if(enviadoSMS){
-                    Intent intent = new Intent(LoginActivity.this, ValidadorActivity.class);
-                    startActivity(intent);
-                    finish();
-                } else {
-                    Toast.makeText(LoginActivity.this, "Problema ao enviar SMS. Tente novamente.", Toast.LENGTH_LONG).show();
-                }
+                Intent intent = new Intent(LoginActivity.this, CadastroUsuarioActivity.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private boolean enviarSMS(String telefone, String mensagem){
-        try{
-            SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(telefone, null, mensagem, null, null);
-            return true;
-        } catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        for(int result : grantResults){
-            if(result == PackageManager.PERMISSION_DENIED){
-                alertaValidacaoPermissao();
-            }
-        }
-    }
-
-    private void alertaValidacaoPermissao(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Permissões Negadas");
-        builder.setMessage("Para utilizar esse app, é necessario aceitar as permissões.");
-        builder.setPositiveButton("CONFIRMAR", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
 }
