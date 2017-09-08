af9c8d3e767e222a071522c3fa224e320babd460
Marcello <mrcllw@Marcellos-iMac.local>
2017-09-08 10:04:09 -0400

    Toolbar e estilo customizado

--- app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
@@ -3,6 +3,10 @@ package com.mrcllw.whatsapp.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +17,7 @@ import com.mrcllw.whatsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonSair;
+    private Toolbar toolbar;
 
     private FirebaseAuth firebaseAuth;
     //private DatabaseReference databaseReference;
@@ -28,16 +32,36 @@ public class MainActivity extends AppCompatActivity {
         //databaseReference = FirebaseDatabase.getInstance().getReference("teste");
         //databaseReference.setValue("teste");
 
-        buttonSair = (Button) findViewById(R.id.buttonSair);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        buttonSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                firebaseAuth.signOut();
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        toolbar.setTitle("WhatsApp");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSair:
+                deslogarUsuario();
+                return true;
+            case R.id.itemConfiguracoes :
+                return true;
+            default :
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUsuario(){
+        firebaseAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
