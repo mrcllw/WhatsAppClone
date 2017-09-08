6a7311d3abe4ad3a0b08804a3520c95db768c6b0
Marcello <mrcllw@Marcellos-iMac.local>
2017-09-08 10:51:15 -0400

    Sliding Tabs

--- app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
+++ app/src/main/java/com/mrcllw/whatsapp/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mrcllw.whatsapp.activity;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,10 +16,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.mrcllw.whatsapp.R;
+import com.mrcllw.whatsapp.adapter.TabAdapter;
+import com.mrcllw.whatsapp.helper.SlidingTabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private SlidingTabLayout stlTabs;
+    private ViewPager viewPager;
 
     private FirebaseAuth firebaseAuth;
     //private DatabaseReference databaseReference;
@@ -33,9 +39,18 @@ public class MainActivity extends AppCompatActivity {
         //databaseReference.setValue("teste");
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        stlTabs = (SlidingTabLayout) findViewById(R.id.stlTabs);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
 
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
+
+        TabAdapter tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+
+        stlTabs.setDistributeEvenly(true);
+        stlTabs.setSelectedIndicatorColors(ContextCompat.getColor(this, R.color.colorAccent));
+        stlTabs.setViewPager(viewPager);
     }
 
     @Override
---
+++ app/src/main/java/com/mrcllw/whatsapp/adapter/TabAdapter.java
@@ -0,0 +1,47 @@
+package com.mrcllw.whatsapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.mrcllw.whatsapp.fragment.ContatosFragment;
+import com.mrcllw.whatsapp.fragment.ConversasFragment;
+
+/**
+ * Created by mrcllw on 08/09/17.
+ */
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private String[] tituloAbas = {"CONVERSAS" ,"CONTATOS"};
+
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+
+        switch(position) {
+            case 0:
+                fragment = new ConversasFragment();
+                break;
+            case 1:
+                fragment = new ContatosFragment();
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return tituloAbas.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tituloAbas[position];
+    }
+}
